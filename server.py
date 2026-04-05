7bf3a9b7a1bcc4e1db3aa2be5139575bb25fe38f
sushmadadi04 <sushmadadi0417@gmail.com>
2026-04-05 12:29:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -104,5 +104,5 @@ def handle_settings():
         return jsonify({"success": True, "settings": settings_db})
     return jsonify(settings_db)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
